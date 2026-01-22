f760c4b8efb3deafdcc00e09dfebb923664ff351
程思双 <eu3059@yunxiao.com>
2026-01-22 18:43:27 +0800

    Initial commit: Electron + Vue project setup

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+// todo 添加一个版本信息的组件
+</script>
+
+<template>
+  <div class="electron-vite">electron-vite</div>
+</template>
+
+<style>
+.electron-vite {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  font-size: 24px;
+  color: #333;
+}
+</style>
